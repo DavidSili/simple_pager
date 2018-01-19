4e50893a027e01641f7377a611ba4196eebbfb86
David Sili <agapetos@gmail.com>
2018-01-19 09:35:08 +0100

    Changed to fixed positioning

--- pager.php
+++ pager.php
@@ -12,35 +12,31 @@
  */
 function pager($total_pages, $page) {
 	$elements = array();
-	if ($page != 0) {
-		$elements[] = '<div class="pager-element" data-page="0"><i class="fa fa-backward"></i></div>';
-		$elements[] = '<div class="pager-element" data-page="'.($page - 1).'"><i class="fa fa-chevron-left"></i></div>';
-	}
+	$slot = array(
+		($page == 0) ? ' pager-hidden' : '',
+		($page == 0) ? ' pager-hidden' : '',
+		($page <= 2) ? ' pager-hidden' : '',
+		(($page - 2) < 0) ? ' pager-hidden' : '',
+		(($page - 1) < 0) ? ' pager-hidden' : '',
+		'',
+		(($page + 1) >= $total_pages) ? ' pager-hidden' : '',
+		(($page + 2) >= $total_pages) ? ' pager-hidden' : '',
+		(($total_pages - $page) <= 3) ? ' pager-hidden' : '',
+		($page == ($total_pages - 1)) ? ' pager-hidden' : '',
+		($page == ($total_pages - 1)) ? ' pager-hidden' : '',
+	);
 	
-	if ($page > 2) {
-		$elements[] = '<div class="pager-element">...</div>';
-	}
-	if (($page - 2) >= 0) {
-		$elements[] = '<div class="pager-element" data-page="'.($page - 2).'">'.($page - 1).'</div>';
-	}
-	if (($page - 1) >= 0) {
-		$elements[] = '<div class="pager-element" data-page="'.($page - 1).'">'.$page.'</div>';
-	}
-	$elements[] = '<div class="pager-element active">'.($page + 1).'</div>';
-	if (($page + 1) < $total_pages) {
-		$elements[] = '<div class="pager-element" data-page="'.($page + 1).'">'.($page + 2).'</div>';
-	}
-	if (($page + 2) < $total_pages) {
-		$elements[] = '<div class="pager-element" data-page="'.($page + 2).'">'.($page + 3).'</div>';
-	}
-	if (($total_pages - $page) > 3) {
-		$elements[] = '<div class="pager-element">...</div>';
-	}
-	
-	if ($page != ($total_pages - 1)) {
-		$elements[] = '<div class="pager-element" data-page="'.($page + 1).'"><i class="fa fa-chevron-right"></i></div>';
-		$elements[] = '<div class="pager-element" data-page="'.($total_pages - 1).'"><i class="fa fa-forward"></i></div>';
-	}
+	$elements[] = '<div class="pager-element'.$slot[0].'" data-page="0"><i class="fa fa-backward"></i></div>';
+	$elements[] = '<div class="pager-element'.$slot[1].'" data-page="'.($page - 1).'"><i class="fa fa-chevron-left"></i></div>';
+	$elements[] = '<div class="pager-element'.$slot[2].'">...</div>';
+	$elements[] = '<div class="pager-element'.$slot[3].'" data-page="'.($page - 2).'">'.($page - 1).'</div>';
+	$elements[] = '<div class="pager-element'.$slot[4].'" data-page="'.($page - 1).'">'.$page.'</div>';
+	$elements[] = '<div class="pager-element active'.$slot[5].'">'.($page + 1).'</div>';
+	$elements[] = '<div class="pager-element'.$slot[6].'" data-page="'.($page + 1).'">'.($page + 2).'</div>';
+	$elements[] = '<div class="pager-element'.$slot[7].'" data-page="'.($page + 2).'">'.($page + 3).'</div>';
+	$elements[] = '<div class="pager-element'.$slot[8].'">...</div>';
+	$elements[] = '<div class="pager-element'.$slot[9].'" data-page="'.($page + 1).'"><i class="fa fa-chevron-right"></i></div>';
+	$elements[] = '<div class="pager-element'.$slot[10].'" data-page="'.($total_pages - 1).'"><i class="fa fa-forward"></i></div>';
 	
 	return '<div class="list_pager">'.implode('', $elements).'</div>';
 }
